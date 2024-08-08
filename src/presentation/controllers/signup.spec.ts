0895f7e819607bbb2df8376c7ae42ae2a0798fb7
LucasValim <lborges801@gmail.com>
2024-08-08 09:23:38 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,9 +4,11 @@ describe('SingUpController', () => {
   test('Should return 400 if no name is provided ', () => {
     const sut = new SingUpController()
     const httpRequest = {
-      emai: 'any_email@mail.com',
-      password: 'any_password',
-      password_confirmation: 'any_password'
+      body: {
+        emai: 'any_email@mail.com',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
@@ -16,9 +18,11 @@ describe('SingUpController', () => {
   test('Should return 400 if no email is provided ', () => {
     const sut = new SingUpController()
     const httpRequest = {
-      name: 'any_name',
-      password: 'any_password',
-      password_confirmation: 'any_password'
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
