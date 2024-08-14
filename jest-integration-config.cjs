1dc420f7cdf88809269a796f17d6a7b7f682e7b2
LucasValim <lborges801@gmail.com>
2024-08-13 21:28:31 -0300

    refactor: fix import and extention jest config files

--- jest-integration-config.cjs
+++ jest-integration-config.cjs
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require('./jest.config')
-config.testMatch = ['**/*.spec.ts']
+const config = require('./jest.config.cjs')
+config.testMatch = ['**/*.test.ts']
 module.exports = config
